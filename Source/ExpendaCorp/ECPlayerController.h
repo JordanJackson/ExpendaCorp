900ac985ff6cfc86fe593ea6ae0497d2edc06df5
Jordan Jackson <jordanpatrickjackson@gmail.com>
2024-12-04 21:28:36 -0400

    Initial Project Setup

---
+++ Source/ExpendaCorp/ECPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "ECPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EXPENDACORP_API AECPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
